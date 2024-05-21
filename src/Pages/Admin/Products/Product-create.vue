4d2c78975eb1cf25d927b90c0f56b2c8a4596b14
Mudassar Shoukat <mudassarshoukat44@gmail.com>
2024-05-21 19:05:39 +0500

    working of confirm delete button

--- src/Pages/Admin/Products/Product-create.vue
+++ src/Pages/Admin/Products/Product-create.vue
@@ -70,7 +70,7 @@
           <!-- Delete Button -->
           <td class="border border-gray-300">
             <button
-              @click="showConfirmDelete"
+              @click="Delete"
               class="bg-slate-200 px-[20px] py-[4px] rounded-[8px] text-[red] border border-[#ccd1d8] hover:underline hover:bg-gray-100"
             >
               Delete
@@ -84,59 +84,66 @@
     <p class="text-center text-[red]">No products found...</p>
   </div>
 
-<!-- Popup show clik delete button -->
+  <!-- Popup show clik delete button -->
 
   <div
     v-if="showConfirmationPopup"
     class="fixed inset-0 bg-gray-500 bg-opacity-50 flex justify-center items-center"
   >
-    <div class="bg-white px-8 py-5 rounded-lg ">
+    <div class="bg-white px-8 py-5 rounded-lg">
       <h3 class="text-lg font-medium text-center mb-3">
         Are you sure you want to delete this product?
       </h3>
-     
+
       <div class="flex justify-between mt-4">
         <button
           @click="showConfirmationPopup = false"
-          class="px-4 py-2 bg-gray-200 rounded-md hover:bg-gray-300"
+          class="px-4 py-2 bg-gray-300 rounded-md hover:bg-[#cbcccd]"
         >
           Cancel
         </button>
         <button
-        @click="deleteProductfield"
+        @click="confirmDelete"
           class="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
         >
           Delete
         </button>
       </div>
     </div>
-    
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
-import { collection, getDocs,deleteField ,doc,updateDoc } from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 import { db } from "../../../Firebase/FB-Database";
 
 const products = ref([]);
 const showConfirmationPopup = ref(false);
-const productId = ref(null)
 
+async function fetchProducts() {
+  try {
+    const querySnapshot = await getDocs(collection(db, "Products"));
+    products.value = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+}
 
-onMounted(async () => {
-  const querySnapshot = await getDocs(collection(db, "Products"));
-  products.value = querySnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
+const Delete = () => {
+  console.log("delete");
 
-  console.log("new array", products.value);
-});
-
-
-const showConfirmDelete = () => {
   showConfirmationPopup.value = true;
 };
 
+
+const confirmDelete=()=>{
+  console.log("confirm Delete");
+}
+
+
+onMounted(fetchProducts);
 </script>
