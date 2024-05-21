23e201f8b6df7a56b6b956a795c30df1c33fe038
Mudassar Shoukat <mudassarshoukat44@gmail.com>
2024-05-21 16:20:28 +0500

    Delete Field

--- src/Pages/Admin/Products/Product-create.vue
+++ src/Pages/Admin/Products/Product-create.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- Button click push page in Form component -->
   <div class="flex justify-center mb-7">
     <button
       type="button"
@@ -10,48 +11,118 @@
     </button>
   </div>
 
-  <div v-if="products.length > 0">
-    <table class=" border border-collapse shadow-sm w-auto m-[30px]">
-      <tr>
-        <th
-          class="px-4 py-2 text-center text-sm  bg-[#f4f1f1] border border-[#e3dddd] text-[#524f4f] underline"
-        >
-          Title
-        </th>
-        <th
-          class="px-4 py-2 text-center text-sm  bg-[#f4f1f1] border border-[#e3dddd] text-[#524f4f] underline"
-        >
-          Price
-        </th>
-        <th
-          class="px-4 py-2 text-center text-sm  bg-[#f4f1f1] border border-[#e3dddd] text-[#524f4f] underline"
-        >
-          Stock
-        </th>
-      </tr>
-
+  <!-- Table outer div -->
+  <div
+    class="overflow-x-auto sm:rounded-lg border m-8"
+    v-if="products.length > 0"
+  >
+    <table class="w-full text-sm text-center text-gray-500 border bg-[#F1F5F9]">
+      <!-- Table Header -->
+      <thead class="text-xs text-gray-700 uppercase underline">
+        <tr class="bg-[#e9ecf0]">
+          <th class="px-6 py-3">Title</th>
+          <th class="px-6 py-3">Price</th>
+          <th class="px-6 py-3">Stock</th>
+          <th class="px-6 py-3">Brand</th>
+          <th class="px-6 py-3">Rating</th>
+          <th class="px-6 py-3">Category</th>
+          <th class="px-6 py-3">Discount</th>
+          <th class="px-6 py-3">Discription</th>
+          <th class="px-6 py-3">Edit</th>
+          <th class="px-6 py-3">Delete</th>
+        </tr>
+      </thead>
+      <!-- Table Body -->
       <tbody>
         <tr v-for="product in products" :key="product.id">
-          <td class="px-4 py-2 border border-gray-200">{{ product.title }}</td>
-          <td class="px-4 py-2 border border-gray-200">{{ product.price }}</td>
-          <td class="px-4 py-2 border border-gray-200">{{ product.stock }}</td>
-    
-
+          <td class="px-4 py-2 border border-gray-300">
+            {{ product.title }}
+          </td>
+          <td class="px-4 py-2 border border-gray-300">
+            {{ product.price }}
+          </td>
+          <td class="px-4 py-2 border border-gray-300">
+            {{ product.stock }}
+          </td>
+          <td class="px-4 py-2 border border-gray-300">
+            {{ product.brand }}
+          </td>
+          <td class="px-4 py-2 border border-gray-300">
+            {{ product.rating }}
+          </td>
+          <td class="px-4 py-2 border border-gray-300">
+            {{ product.category }}
+          </td>
+          <td class="px-4 py-2 border border-gray-300">
+            {{ product.discountPercentage }}
+          </td>
+          <td class="px-4 py-2 border border-gray-300">
+            {{ product.description }}
+          </td>
+          <!-- Edit Button -->
+          <td class="border border-gray-300">
+            <button
+              class="bg-slate-200 px-[25px] py-[4px] rounded-[8px] text-[blue] border border-[#ccd1d8] hover:underline hover:bg-gray-100"
+            >
+              Edit
+            </button>
+          </td>
+          <!-- Delete Button -->
+          <td class="border border-gray-300">
+            <button
+              @click="showConfirmDelete"
+              class="bg-slate-200 px-[20px] py-[4px] rounded-[8px] text-[red] border border-[#ccd1d8] hover:underline hover:bg-gray-100"
+            >
+              Delete
+            </button>
+          </td>
         </tr>
       </tbody>
     </table>
   </div>
   <div v-else>
-    <p class="text-center text-gray-500">No products found...</p>
+    <p class="text-center text-[red]">No products found...</p>
+  </div>
+
+<!-- Popup show clik delete button -->
+
+  <div
+    v-if="showConfirmationPopup"
+    class="fixed inset-0 bg-gray-500 bg-opacity-50 flex justify-center items-center"
+  >
+    <div class="bg-white px-8 py-5 rounded-lg ">
+      <h3 class="text-lg font-medium text-center mb-3">
+        Are you sure you want to delete this product?
+      </h3>
+     
+      <div class="flex justify-between mt-4">
+        <button
+          @click="showConfirmationPopup = false"
+          class="px-4 py-2 bg-gray-200 rounded-md hover:bg-gray-300"
+        >
+          Cancel
+        </button>
+        <button
+        @click="deleteProductfield"
+          class="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs,deleteField ,doc,updateDoc } from "firebase/firestore";
 import { db } from "../../../Firebase/FB-Database";
 
 const products = ref([]);
+const showConfirmationPopup = ref(false);
+const productId = ref(null)
+
 
 onMounted(async () => {
   const querySnapshot = await getDocs(collection(db, "Products"));
@@ -62,4 +133,10 @@ onMounted(async () => {
 
   console.log("new array", products.value);
 });
+
+
+const showConfirmDelete = () => {
+  showConfirmationPopup.value = true;
+};
+
 </script>
